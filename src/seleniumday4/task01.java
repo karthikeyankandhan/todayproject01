8ce8084fe32fc8aee4facaba3073cf340396ccd3
karthikeyankandhan <karthikeyankandhan@gmail.com>
2019-09-26 12:00:54 +0530

    next add

--- src/seleniumday4/task01.java
+++ src/seleniumday4/task01.java
@@ -8,7 +8,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class task01 {
 
 	public static void main(String[] args) {
-		
+		int a=10;
+		int b=10;
 		System.setProperty("wedriver.chrome.driver","C:\\Users\\pr soft\\eclipse-workspace\\karthikeyankandhan\\seleniumday4\\driverPath\\chromedriverVersion76.1.exe");
 	
 		WebDriver driver = new ChromeDriver();
